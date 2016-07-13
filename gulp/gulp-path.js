194afe86d8398335ab60e4c97bea4648e713646c
Emi Gerez <Emi Gerez>
2016-07-12 21:09:34 -0300

    example

--- gulp/gulp-path.js
+++ gulp/gulp-path.js
@@ -9,13 +9,12 @@ module.exports = {
     assetsDist: './app/assets/dist',
     jsFiles: './app/assets/js/**/*.js',
     cssBuild: [
-        './app/assets/components/bootstrap/dist/css/bootstrap.css',
         './app/assets/components/animate.css/animate.css',
         './app/assets/css/**/*.css'
     ],
     jsBuild: [
         './app/assets/components/jquery/dist/jquery.js',
-        './app/assets/components/bootstrap/dist/js/bootstrap.js',
+        './app/assets/components/bootstrap-sass/assets/javascripts/bootstrap.js',
         './app/assets/js/**/*.js'
     ]
 };
